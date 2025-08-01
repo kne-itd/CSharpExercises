e2c30c2ad519d3b192f4ed18f292e9f421f006b5
kne-itd <kne@sde.dk>
2025-08-01 10:53:58 +0200

    Create StudentCode.cs

---
+++ StudentCode.cs
@@ -0,0 +1,29 @@
+public static class StudentCode
+{
+    public static string Greet(string name) { /* ... */ }
+    public static int Sum(int a, int b) { /* ... */ }
+    public static bool IsEven(int number) { /* ... */ }
+    public static int Max(int a, int b) { /* ... */ }
+    public static double Average(int a, int b) { /* ... */ }
+    public static bool IsPrime(int number) { /* ... */ }
+    public static int CountOccurrences(string text, char letter) { /* ... */ }
+    public static int[] Reverse(int[] array) { /* ... */ }
+    public static int CountVowels(string word) { /* ... */ }
+    public static string FizzBuzz(int number) { /* ... */ }
+
+    public static int Min(int[] numbers) { /* ... */ }
+    public static int CountInArray(int[] arr, int value) { /* ... */ }
+    public static bool IsPalindrome(string text) { /* ... */ }
+    public static int Factorial(int n) { /* ... */ }
+    public static int SecondLargest(int[] numbers) { /* ... */ }
+    public static double CelsiusToFahrenheit(double celsius) { /* ... */ }
+    public static bool IsWeekend(DateTime date) { /* ... */ }
+    public static int SumAll(params int[] numbers) { /* ... */ }
+    public static string LongestWord(string sentence) { /* ... */ }
+
+    public static string GetComment(Grade grade)
+    {
+        // Grade er defineret i test-projektet
+        throw new NotImplementedException();
+    }
+}
